18007525690a5d79b171e93918b595085542c2c5
Bagher hosseini <bagherhosseini03@gmail.com>
2023-04-14 12:30:24 +0200

    updated validate test tests valid and invalid data

--- src/test/modules/validate.test.js
+++ src/test/modules/validate.test.js
@@ -1,89 +1,54 @@
 const { validate } = require('../../modules/validate');
-const Joi = require('joi');
 
-// Mock the res object to capture responses
+describe('Validate function tests', () => {
+  test('should pass with valid input', () => {
+    const val = [
+      { name: 'John Doe', type: 'string', min: 3, max: 50, required: true },
+      { age: 30, type: 'number', min: 18, max: 120, required: true },
+    ];
 
-const resMock = {
-    status: jest.fn().mockReturnThis(),
-    json: jest.fn(),
-};
+    const req = {
+      body: {
+        name: 'John Doe',
+        age: 30,
+      },
+    };
 
-// Helper function to create a request object
-const createRequestMock = (body) => ({ body });
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
 
-describe('Validator tests', () => {
-    beforeEach(() => {
-        jest.resetAllMocks();
-    });
+    const validationResult = validate(val, req, res);
 
-    test('should return "success" when all validation rules pass', () => {
-        const validationSchema = [
-            { type: 'string', min: 1, max: 5, required: true, fieldName: 'name' },
-        ];
-        const req = createRequestMock({ name: 'John' });
+    expect(validationResult).toBe(true); 
+    expect(res.status).not.toHaveBeenCalled();
+    expect(res.json).not.toHaveBeenCalled();
+  });
 
-        const result = validate(validationSchema, req, resMock);
-        expect(result).toEqual(['success']);
-    });
+  test('should fail with invalid input', () => {
+    const val = [
+      { name: 'J', type: 'string', min: 3, max: 50, required: true },
+      { name: 200, type: 'number', min: 18, max: 120, required: true },
+    ];
 
-    test('should return error message when required field is missing', () => {
-        const validationSchema = [
-            { type: 'string', min: 1, max: 5, required: true, fieldName: 'name' },
-        ];
-        const req = createRequestMock({});
+    const req = {
+      body: {
+        name: 'J', // Invalid name (length is less than 3)
+        age: 200, // Invalid age (greater than 120)
+      },
+    };
 
-        const result = validate(validationSchema, req, resMock);
-        expect(result).toContain('"fieldName" is required');
-        expect(resMock.status).toHaveBeenCalledWith(400);
-        expect(resMock.json).toHaveBeenCalledWith('"fieldName" is required');
-    });
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+      success: jest.fn(),
+    };
 
-    test('should return error message when value is below minimum', () => {
-        const validationSchema = [
-            { type: 'number', min: 5, fieldName: 'age' },
-        ];
-        const req = createRequestMock({ age: 3 });
+    const validationResult = validate(val, req, res);
 
-        const result = validate(validationSchema, req, resMock);
-        expect(result).toContain('"age" must be larger than or equal to 5');
-        expect(resMock.status).toHaveBeenCalledWith(400);
-        expect(resMock.json).toHaveBeenCalledWith('"age" must be larger than or equal to 5');
-    });
-
-    test('should return error message when value is above maximum', () => {
-        const validationSchema = [
-            { type: 'string', max: 5, fieldName: 'name' },
-        ];
-        const req = createRequestMock({ name: 'Longer name' });
-
-        const result = validate(validationSchema, req, resMock);
-        expect(result).toContain('"name" length must be less than or equal to 5 characters long');
-        expect(resMock.status).toHaveBeenCalledWith(400);
-        expect(resMock.json).toHaveBeenCalledWith('"name" length must be less than or equal to 5 characters long');
-    });
-
-    test('should return error message for invalid extra properties', () => {
-        const validationSchema = [
-            { type: 'string', min: 1, max: 5, fieldName: 'name' },
-        ];
-        const req = createRequestMock({ name: 'John', invalidProp: 'Invalid' });
-
-        const result = validate(validationSchema, req, resMock);
-        expect(result).toContain('Invalid properties: invalidProp');
-        expect(resMock.status).toHaveBeenCalledWith(400);
-        expect(resMock.json).toHaveBeenCalledWith('Invalid properties: invalidProp');
-    });
-
-    test('should not validate fields not in request body', () => {
-      const validationSchema = [
-          { type: 'string', min: 1, max: 5, fieldName: 'name' },
-          { type: 'number', min: 1, max: 5, fieldName: 'age' },
-      ];
-      const req = createRequestMock({ name: 'John' });
-  
-      const result = validate(validationSchema, req, resMock);
-      expect(result).toEqual(['success']);
-      expect(resMock.status).not.toHaveBeenCalled();
-      expect(resMock.json).not.toHaveBeenCalled();
-    });
-})
+    expect(validationResult).toBe(false); // Update this line
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.json).toHaveBeenCalledTimes(1);
+  });
+});
