fffc0ac658cfdfe8a2aca15deb38f04864f8b4a7
gufr2201 <gfranchell@gmail.com>
2023-04-17 14:38:24 +0200

    create loginController with validation and cookie

---
+++ src/controllers/authenticationController/loginController.js
@@ -0,0 +1,52 @@
+require('dotenv').config();
+const bcrypt = require('bcrypt');
+const url = process.env.URL;
+const jwt = require('jsonwebtoken')
+const secret = process.env.SECRET;
+const { validate } = require('../../modules/validate');
+const mongodb = require('../../modules/databaseQuery');
+
+exports.login = async function login(req, res) {
+    const { username, password } = req.body;
+    const data = [
+        { username: username, type: 'string', min: 3, max: 50, required: true },
+        { password: password, type: 'string', min: 3, max: 50, required: true }, 
+    ];
+
+    const validationResult = validate(data, req, res);
+    if (!validationResult) {
+        // Validation failed, return early
+        return;
+    }
+    
+    try {
+        
+        const user = {username: username}
+        const findQuery = await mongodb(url, 'BeeHive', 'users')
+        const findResult = await findQuery.findOne(user);
+        const storedPassword = findResult.password;
+        const isEqual = bcrypt.compareSync(password, storedPassword);
+
+        if (!isEqual) {
+                         res.status(401).json({message: 'Invalid login'})
+        
+                        return;
+              }
+
+              const token = jwt.sign({username}, secret, {expiresIn: 120});
+            res.cookie('token', token, {
+                maxAge: 3600000,
+                sameSite: 'none',
+                // Secure är just nu buggat för Postman, använd inte secure: true för Postman.
+                secure: false,
+                httpOnly: false
+            });
+
+            res.status(200).json('Login successful');
+      
+        return;
+    } catch (error) {
+        
+        res.status(500).json({ message: 'Invalid credentials' });
+    }
+};
