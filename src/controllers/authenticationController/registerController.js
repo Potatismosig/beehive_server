8f38719edf3182d6ceb1d7df0df0b507b797c351
Bagher hosseini <bagherhosseini03@gmail.com>
2023-04-17 13:41:00 +0200

    cleaned up register contoller

--- src/controllers/authenticationController/registerController.js
+++ src/controllers/authenticationController/registerController.js
@@ -1,12 +1,7 @@
-const express = require('express');
 require('dotenv').config();
-const server = express();
-server.use(express.json());
-const cookieParser = require('cookie-parser');
-server.use(cookieParser());
 const bcrypt = require('bcrypt');
-const secret = process.env.SECRET;
 const url = process.env.URL;
+
 const { validate } = require('../../modules/validate');
 const mongodb = require('../../modules/databaseQuery');
 
@@ -14,29 +9,31 @@ exports.register = async function register(req, res) {
     const { username, password } = req.body;
     const data = [
         { username: username, type: 'string', min: 3, max: 50, required: true },
-        { password: password, type: 'string', min: 3, max: 50, required: true },
+        { password: password, type: 'string', min: 3, max: 50, required: true }, 
     ];
+
     const validationResult = validate(data, req, res);
     if (!validationResult) {
         // Validation failed, return early
         return;
     }
-    const salt = bcrypt.genSaltSync(10);
-    const hashed = bcrypt.hashSync(password, salt);
-    const user = { username: username, password: hashed };
+    
     try {
+        const salt = bcrypt.genSaltSync(10);
+        const hashed = bcrypt.hashSync(password, salt);
+
+        const user = { username: username, password: hashed };
+
         const insertQuery = await mongodb(url, 'BeeHive', 'users')
         const insertResult = await insertQuery.insertOne(user);
+        
         res.status(201).json(insertResult);
+        return;
     } catch (error) {
-        console.log(error)
         if (error.code === 11000) {
             res.status(409).json('' + error.keyValue.username + ' already exists');
             return;
         };
         res.status(500).json({ message: 'Something went wrong' });
     }
-
-
-
 };
