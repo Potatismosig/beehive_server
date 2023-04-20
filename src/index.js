23007bb40ffce4dc4eaf4ef5f2445357c7e628f3
Bagher hosseini <bagherhosseini03@gmail.com>
2023-04-20 16:14:33 +0200

    added cookieParser

--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
 const express = require('express');
 require('dotenv').config();
 const server = express();
+const cookieParser = require('cookie-parser');
+
 server.use(express.json());
+server.use(cookieParser());
+
 const { checkAuthentication } = require('./middleware/checkAuthentication');
-
-
 const { authenticationRoute } = require('./routes/authenticationRoute');
 
-
-// express.json() gör samma sak som vi gjorde i våran egna middleware.
-server.use(express.json());
-
-
 server.use('/auth', authenticationRoute);
-server.get('/', checkAuthentication, (req, res) => { res.status(200).json('Junie is the best!') });
 
 
 server.listen(5050);
