e425b5c447ad7708b093b1e5755f4bcf5527a0b0
Jirawan <jirawan.sangthong@yh.nackademin.se>
2023-04-20 15:59:05 +0200

    created a middleware to check cookie

---
+++ src/middleware/checkAuthentication.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken');
+const secret = process.env.SECRET;
+
+exports.checkAuthentication = function checkAuthentication(req, res, next) {
+    // const { token } = req.cookies;
+    console.log(req);
+    try {
+        const loggedInUser = jwt.verify(token, secret);
+        req.loggedInUser = loggedInUser;
+        next();
+    }
+    catch (error) {
+        res.status(401).json('Authentication error: jwt expired');
+    }
+}
