f5d153c913dabe7829ce1596ba5101677e9b0e7b
Bagher hosseini <bagherhosseini03@gmail.com>
2023-04-20 16:15:11 +0200

    imported token

--- src/middleware/checkAuthentication.js
+++ src/middleware/checkAuthentication.js
@@ -2,8 +2,7 @@ const jwt = require('jsonwebtoken');
 const secret = process.env.SECRET;
 
 exports.checkAuthentication = function checkAuthentication(req, res, next) {
-    // const { token } = req.cookies;
-    console.log(req);
+    const token = req.cookies.token;
     try {
         const loggedInUser = jwt.verify(token, secret);
         req.loggedInUser = loggedInUser;
