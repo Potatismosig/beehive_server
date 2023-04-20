19f0eb68469389de52752182aa172aff145ad85a
Jirawan <jirawan.sangthong@yh.nackademin.se>
2023-04-20 15:58:22 +0200

    imported middleware

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 require('dotenv').config();
 const server = express();
 server.use(express.json());
+const { checkAuthentication } = require('./middleware/checkAuthentication');
 
 
 const { authenticationRoute } = require('./routes/authenticationRoute');
@@ -12,6 +13,7 @@ server.use(express.json());
 
 
 server.use('/auth', authenticationRoute);
+server.get('/', checkAuthentication, (req, res) => { res.status(200).json('Junie is the best!') });
 
 
 server.listen(5050);
