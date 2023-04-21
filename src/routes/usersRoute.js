989ac8aeeb0363930b781156fc7c59a452d84f3b
Jirawan <jirawan.sangthong@yh.nackademin.se>
2023-04-21 12:37:52 +0200

    removed followUser

--- src/routes/usersRoute.js
+++ src/routes/usersRoute.js
@@ -1,8 +1,5 @@
 const express = require('express');
-const { followUser } = require('../controllers/usersController/followUserController.js');
 
 const usersRoute = express.Router();
-
-usersRoute.post('/follow', followUser);
 
 exports.usersRoute = usersRoute;
