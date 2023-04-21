3f893ee4daed644c8735209f652aa257593f4f5a
Jirawan <jirawan.sangthong@yh.nackademin.se>
2023-04-21 12:27:38 +0200

    imported users route

--- src/index.js
+++ src/index.js
@@ -7,9 +7,13 @@ server.use(express.json());
 server.use(cookieParser());
 
 const { checkAuthentication } = require('./middleware/checkAuthentication');
+
 const { authenticationRoute } = require('./routes/authenticationRoute');
+const { usersRoute } = require('./routes/usersRoute');
 
 server.use('/auth', authenticationRoute);
+server.use('/users', checkAuthentication, usersRoute);
+
 
 
 server.listen(5050);
