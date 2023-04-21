75c454e8a68111f7d30dc313d27c5c7ab4117d46
Bagher hosseini <bagherhosseini03@gmail.com>
2023-04-21 11:42:43 +0200

    imported usersRoute

--- src/index.js
+++ src/index.js
@@ -7,10 +7,13 @@ server.use(express.json());
 server.use(cookieParser());
 
 const { checkAuthentication } = require('./middleware/checkAuthentication');
+
 const { authenticationRoute } = require('./routes/authenticationRoute');
 const { postsRoute } = require('./routes/postsRoute');
+const { usersRoute } = require('./routes/usersRoute');
 
 server.use('/auth', authenticationRoute);
 server.use('/posts', checkAuthentication, postsRoute);
+server.use('/users', checkAuthentication, usersRoute);
 
 server.listen(5050);
