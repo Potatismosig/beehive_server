39a8bb57bf2fcbf333c7a091693aa5b8283779fa
Bagher hosseini <bagherhosseini03@gmail.com>
2023-04-23 23:18:21 +0200

    added create & delete route to avoid

--- src/routes/postsRoute.js
+++ src/routes/postsRoute.js
@@ -1,7 +1,13 @@
 const express = require('express');
 
+const { createPost } = require('../controllers/postController/createPostController');
 const { getUserPosts } = require('../controllers/postController/getUserPosts');
+const { deletePost } = require('../controllers/postController/deletePostController');
+
 const postsRoute = express.Router();
 
 postsRoute.get('/userposts', getUserPosts);
+postsRoute.post('/create', createPost);
+postsRoute.delete('/delete', deletePost);
+
 exports.postsRoute = postsRoute;
