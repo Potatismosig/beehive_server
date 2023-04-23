b138bcce995ff33b5db1969fe024e670e153e12f
Jirawan <jirawan.sangthong@yh.nackademin.se>
2023-04-23 13:32:08 +0200

    created controller for getUserPosts

---
+++ src/controllers/postController/getUserPosts.js
@@ -0,0 +1,17 @@
+const mongodb = require('../../modules/databaseQuery');
+const { getUsername } = require('../../modules/getUsername');
+require('dotenv').config();
+const url = process.env.URL;
+
+exports.getUserPosts = async function getUserPosts(req, res) {
+    const username = getUsername(req);
+    try {
+        const user = { username: username };
+        const collection = await mongodb(url, 'BeeHive', 'posts');
+        const findQuery = await collection.find(user, true);
+        res.status(200).json(findQuery);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json('Internal error');
+    }
+};
