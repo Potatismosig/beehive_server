be565c61c3df1984a26116efe3062a204eed088e
gufr2201 <gfranchell@gmail.com>
2023-04-24 15:36:58 +0200

    added a route for profilePage

--- src/routes/usersRoute.js
+++ src/routes/usersRoute.js
@@ -3,11 +3,13 @@ const express = require('express');
 const { getUserInfo } = require ('../controllers/userController/getUserInfoController.js');
 const { followUser } = require('../controllers/userController/followUserController.js');
 const { getAllUsers } = require('../controllers/userController/getAllUsersController.js');
+const { profilePage } = require('../controllers/userController/profilePageController.js')
 
 const usersRoute = express.Router();
 
 usersRoute.get('/user', getUserInfo);
 usersRoute.get('/allUsers', getAllUsers);
 usersRoute.post('/follow', followUser);
+usersRoute.post('/profile', profilePage)
 
 exports.usersRoute = usersRoute;
