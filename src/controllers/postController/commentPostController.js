fcba4ba2983fc5307433b08e24611762770a51bb
gufr2201 <gfranchell@gmail.com>
2023-04-25 10:27:11 +0200

    finalize commenting on posts

--- src/controllers/postController/commentPostController.js
+++ src/controllers/postController/commentPostController.js
@@ -3,6 +3,7 @@ const mongodb = require('../../modules/databaseQuery');
 const { validate } = require('../../modules/validate');
 require('dotenv').config();
 const url = process.env.URL;
+const { ObjectId } = require('mongodb');
 
 exports.commentPost = async function commentPost(req, res){
    try {
@@ -22,7 +23,7 @@ exports.commentPost = async function commentPost(req, res){
 
     // Find the post with postId
     const postsQuery = await mongodb(url, 'BeeHive', 'posts');
-    const post = await postsQuery.findOne({ _id: mongodb.ObjectID(postId) });
+    const post = await postsQuery.findOne({ _id: new ObjectId(postId) });
 
     if (!post) {
         res.status(404).json({ message: 'Post not found' });
@@ -37,61 +38,18 @@ exports.commentPost = async function commentPost(req, res){
     });
 
     // Update the post with the new comment
-    const updateResult = await postsQuery.updateOne({ _id: mongodb.ObjectID(postId) }, post);
-
+    const updateResult = await postsQuery.updateOne(
+        { _id: new ObjectId(postId) },
+        { $push: { comments: { username, comment: comments } } }
+      );
     if (updateResult.modifiedCount === 1) {
         res.status(201).json({ message: 'Comment added successfully' });
     } else {
         res.status(500).json({ message: 'Something went wrong' });
     }
    }  catch (error) {
-    res.status(500).json({ message: 'Something went wrong' });
+    console.log(error);
+    res.status(500).json(error);
    }
 }
 
-
-
-
-
-// const {getUsername} = require('../../modules/getUsername')
-// const mongodb = require('../../modules/databaseQuery');
-// const { validate } = require('../../modules/validate');
-// require('dotenv').config();
-// const url = process.env.URL;
-
-// exports.commentPost = async function commentPost(req, res){
-//    try {
-//     const username = getUsername(req, res);
-    
-//     const { postId, comments } = req.body;
-//     const data = [
-//         {comments: comments, type: 'string', min: 3, max: 1000, required: true },
-//         { postId: postId, type: 'string', min: 3, max: false, required: true }
-//      ];
-
-//      const validationResult = validate(data, req, res);
-//      if (!validationResult) {
-//         // Validation failed, return early
-//         return;
-//     }
-//  //Find the post with postId
-//  const postsQuery = await mongodb(url, 'BeeHive', 'posts');
-//  const post = await postsQuery.findOne({ _id: mongodb.ObjectID(postId) }, { $set: post});
-//     if(!post) {
-//         return res.status(404).json('Post not found');
-//     }
-//     post.comments.push({
-//         username: username,
-//         text: comments,
-//         timestamp: new Date()
-//     });
-//     const updateResult = await postsQuery.updateOne({ _id: mongodb.ObjectID(postId) }, post);
-
-//     if (updateResult.modifiedCount === 1) {
-//         res.status(201).json('Comment added successfully');
-//     }
-
-//    }  catch (error) {
-//     res.status(500).json('Something went wrong');
-// }
-// }
